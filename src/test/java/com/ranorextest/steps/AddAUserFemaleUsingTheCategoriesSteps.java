aee6b9bd806a8884b000df260265205308cab388
ArtemOAS <oleynik.artem.s@gmail.com>
2015-01-11 15:44:49 +0200

    Update test selenium change user story and  refactor code

--- src/test/java/com/ranorextest/steps/AddAUserFemaleUsingTheCategoriesSteps.java
+++ src/test/java/com/ranorextest/steps/AddAUserFemaleUsingTheCategoriesSteps.java
@@ -7,12 +7,7 @@ import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
-import org.jbehave.core.model.ExamplesTable;
 import org.junit.Assert;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Тёма on 28.12.2014.
